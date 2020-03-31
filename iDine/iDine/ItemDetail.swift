3817005b8ab0524f0aa58a29d06bf5425a660639
SUBNARA <subhashini.narayanaswamy@daimler.com>
2020-04-01 00:24:40 +0530

    Adding the item detail view and making some synthetic changes to the view

---
+++ iDine/iDine/ItemDetail.swift
@@ -0,0 +1,38 @@
+//
+//  ItemDetail.swift
+//  iDine
+//
+//  Created by Narayanaswamy, Subhashini (623) on 01/04/20.
+//  Copyright © 2020 Narayanaswamy, Subhashini (623). All rights reserved.
+//
+
+import SwiftUI
+
+struct ItemDetail: View {
+    
+    var item: MenuItem
+    
+    var body: some View {
+        VStack{
+            ZStack(alignment: .bottomTrailing) {
+                Image(item.mainImage)
+                Text("Photo: \(item.photoCredit)")
+                .padding(4)
+                .background(Color.black)
+                .font(.caption)
+                .foregroundColor(.white)
+                .offset(x: -5, y: -5)
+            }
+            Text(item.description)
+                .padding()
+            Spacer()
+        }
+        .navigationBarTitle(Text(item.name), displayMode: .inline)
+    }
+}
+
+struct ItemDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemDetail(item: MenuItem.example)
+    }
+}
